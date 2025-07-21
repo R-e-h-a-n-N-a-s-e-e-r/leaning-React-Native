9450e46667f96c7091d1f400958c4dd48f7338f0
REHAN-NASEER <rehannaseer32123@gmail.com>
2025-07-21 18:48:53 +0500

    First Component

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
-import { Button, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Button, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import First from './src/Components/First';
 
 const App = () => {
   return (
     <View>
-      <Text style={{
-        fontSize: 20
-      }}>My first React app using Android Studio</Text>
-      <Button title="Press Here!"/>
+      <Text
+        style={{
+          fontSize: 20,
+        }}
+      >
+        My first React app using Android Studio
+      </Text>
+      <Button title="Press Here!" />
+      <First />
     </View>
   );
 };
